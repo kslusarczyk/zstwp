56cbdfe8f12cc9d5c7482a78ecf1a25f5e046056
kchrusci <kchrusci@student.agh.edu.pl>
2017-01-11 00:37:42 +0100

    Alarms sorted by EVENT TIME

--- script.py
+++ script.py
@@ -1,20 +1,28 @@
 from xlrd import *
 import csv
 
+class Alarms_Correlation:
 
-def csv_from_excel():
-    b = open_workbook('HuaweiU2000Export.xlsx')
-    s = b.sheet_by_name('AlarmExport')
-    bc = open('HuaweiU2000Export.csv', 'w')
-    bcw = csv.writer(bc, csv.excel)
-    for row in range(s.nrows):
-        this_row = []
-        for col in range(s.ncols):
-            val = s.cell_value(row, col)
-            if isinstance(val, unicode):
-                val = val.encode('utf8')
-            this_row.append(val)
-        bcw.writerow(this_row)
+    def __init__(self):
+        self.xls_file = 'HuaweiU2000Export_sorted_by_event_time.xlsx'
+        self.csv_name = 'HuaweiU2000Export_sorted_by_event_time.csv'
+        self.sheet_name = 'AlarmExport'
 
 
-# csv_from_excel()
+    def csv_from_excel(self):
+        b = open_workbook(self.xls_file)
+        s = b.sheet_by_name(self.sheet_name)
+        bc = open(self.csv_name, 'w')
+        bcw = csv.writer(bc, csv.excel)
+        for row in range(s.nrows):
+            this_row = []
+            for col in range(s.ncols):
+                val = s.cell_value(row, col)
+                if isinstance(val, unicode):
+                    val = val.encode('utf8')
+                this_row.append(val)
+            bcw.writerow(this_row)
+
+
+alarms_correlation = Alarms_Correlation()
+alarms_correlation.csv_from_excel()
